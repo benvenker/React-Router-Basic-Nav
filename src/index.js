3d36037bd381b714d09fea055fc07d0f94a3c286
Ben Venker <bvemails@gmail.com>
2020-03-12 21:13:23 -0700

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+    <Route exact path="/">
+      Home
+    </Route>
+    <Route exact path="/about">
+      About
+    </Route>
+    <Route exact path="/contact">
+      Contact
+    </Route>
+  </Router>,
+  document.getElementById("root")
+);
